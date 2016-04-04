47f8480e2f22b7feb0a9ef07311e09cb599054f9
Luan Maia <luangmaiadias@gmail.com>
2016-04-04 16:56:12 -0300

    Classe Decl

---
+++ AST/Decl.java
@@ -0,0 +1,13 @@
+package AST;
+
+public class Decl {
+
+    private StmtBlock stmtBlock;
+
+    public Decl(StmtBlock stmtBlock) {
+        stmtBlock = this.stmtBlock;
+    }
+
+    //Gets e Sets
+
+}
